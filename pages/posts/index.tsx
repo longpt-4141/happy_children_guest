763c95c6b6de9aafb2c058ac9630195c96864a6d
Pham Thanh Long <pham.thanh.long@sun-asterisk.com>
2023-05-28 16:36:53 +0700

    fix

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -24,7 +24,7 @@ export const getStaticProps : GetStaticProps<PostsPageProps> = async (
     context : GetStaticPropsContext
 ) => {
 
-    const response = await fetch(`http://localhost:8080/news`)
+    const response = await fetch(`https://js-post-api.herokuapp.com/api/posts?_page=1`)
     const data = await response.json()
     console.log('\nGet static data', data)
     console.log('\nGet static props', context)
